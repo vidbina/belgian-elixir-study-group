b43e591d95e8e163d61878bf1de58d217caadaf1
David Asabina <david@supr.nu>
2016-01-29 14:06:35 +0100

    Damn... Erlang fun becomes Elixir fn :confused:

--- exercises/part1_sequential_elixir/optional/ex01.exs
+++ exercises/part1_sequential_elixir/optional/ex01.exs
@@ -1,9 +1,9 @@
 # GOAL: getting acquainted with the syntax and built-in functions
 
-sum =
+sum = fn(a, b, c) -> a+b+c end
 
 IO.inspect sum.(1, 2, 3) #=> 6
 
-pair_tuple_to_list =
+pair_tuple_to_list = fn({a, b}) -> [a,b] end
 
 IO.inspect pair_tuple_to_list.( { 1234, 5678 } ) #=> [ 1234, 5678 ]
